7534fcf75c0ef9ccd54d322831dbad43d3a3faf3
mattssca <mattsada@gmail.com>
2025-02-27 14:59:40 +0100

    adding dataset analysis

--- analsysis/delta_plots.R
+++ analsysis/delta_plots.R
@@ -80,16 +80,15 @@ plot_deltas <- function(these_predictions = NULL,
   
   my_plot <- ggplot(prediction_scores, aes(x = reorder(sample_id, delta), y = delta, group = subtype, color = subtype)) +
     geom_line() +
-    geom_point(size = 0.5) +
+    geom_point(size = 0.8) +
     scale_color_manual(values = my_palette) +
-    scale_y_reverse(expand = c(0, 0)) +
+    scale_y_reverse(expand = c(0, 0), limits = c(1, 0)) +
     scale_x_discrete(expand = c(0, 0)) +
     labs(title = plot_title,
          x = "Samples",
          y = "Delta") +
     theme(legend.position = c(0.85, 0.3), 
           legend.background = element_rect(fill = alpha('white', 0.5)), 
-          axis.text.y = element_text(color = "black", size = 7),
           axis.ticks.x = element_blank(), 
           axis.text.x = element_blank(),
           axis.ticks.y = element_line(linewidth = 0.4),
@@ -98,100 +97,339 @@ plot_deltas <- function(these_predictions = NULL,
           panel.grid.minor = element_blank(),
           plot.background = element_blank(),
           panel.border = element_rect(colour = "black", fill = NA, linewidth = 0.4),
-          axis.line.x = element_blank(),
-          axis.title.y = element_text(angle = 90, colour = "black", size = 10, vjust = 2))
+          axis.line.x = element_blank())
   
   return(my_plot)
 }
 
-class_5_plot = plot_deltas(these_predictions = pred_tcga,
-                           this_subtype = NULL,
-                           subtype_class = "5_class",
-                           plot_title = "TCGA", 
-                           return_data = FALSE)
-
-class_7_plot = plot_deltas(these_predictions = pred_tcga,
-                           this_subtype = NULL,
-                           subtype_class = "7_class",
-                           plot_title = "TCGA", 
-                           return_data = FALSE)
-
-uro_plot = plot_deltas(these_predictions = pred_tcga,
-                       this_subtype = "Uro",
-                       subtype_class = "5_class",
-                       plot_title = "TCGA", 
-                       return_data = FALSE)
-
-gu_plot = plot_deltas(these_predictions = pred_tcga,
-                      this_subtype = "GU",
-                      subtype_class = "5_class",
-                      plot_title = "TCGA", 
-                      return_data = FALSE)
-
-basq_plot = plot_deltas(these_predictions = pred_tcga,
-                        this_subtype = "BaSq",
+plot_wrapper = function(these_predictions = NULL, 
+                        plot_title = NULL,
+                        out_path = NULL){
+  
+  class_5_plot = plot_deltas(these_predictions = these_predictions,
+                             this_subtype = NULL,
+                             subtype_class = "5_class",
+                             plot_title = plot_title, 
+                             return_data = FALSE)
+  
+  class_7_plot = plot_deltas(these_predictions = these_predictions,
+                             this_subtype = NULL,
+                             subtype_class = "7_class",
+                             plot_title = plot_title, 
+                             return_data = FALSE)
+  
+  uro_plot = plot_deltas(these_predictions = these_predictions,
+                         this_subtype = "Uro",
+                         subtype_class = "5_class",
+                         plot_title = plot_title, 
+                         return_data = FALSE)
+  
+  gu_plot = plot_deltas(these_predictions = these_predictions,
+                        this_subtype = "GU",
                         subtype_class = "5_class",
-                        plot_title = "TCGA", 
+                        plot_title = plot_title, 
                         return_data = FALSE)
+  
+  basq_plot = plot_deltas(these_predictions = these_predictions,
+                          this_subtype = "BaSq",
+                          subtype_class = "5_class",
+                          plot_title = plot_title, 
+                          return_data = FALSE)
+  
+  mes_plot = plot_deltas(these_predictions = these_predictions,
+                         this_subtype = "Mes",
+                         subtype_class = "5_class",
+                         plot_title = plot_title, 
+                         return_data = FALSE)
+  
+  scne_plot = plot_deltas(these_predictions = these_predictions,
+                          this_subtype = "ScNE",
+                          subtype_class = "5_class",
+                          plot_title = plot_title, 
+                          return_data = FALSE)
+  
+  uroa_plot = plot_deltas(these_predictions = these_predictions,
+                          this_subtype = "UroA",
+                          subtype_class = "7_class",
+                          plot_title = plot_title, 
+                          return_data = FALSE)
+  
+  urob_plot = plot_deltas(these_predictions = these_predictions,
+                          this_subtype = "UroB",
+                          subtype_class = "7_class",
+                          plot_title = plot_title, 
+                          return_data = FALSE)
+  
+  uroc_plot = plot_deltas(these_predictions = these_predictions,
+                          this_subtype = "UroC",
+                          subtype_class = "7_class",
+                          plot_title = plot_title, 
+                          return_data = FALSE)
 
-mes_plot = plot_deltas(these_predictions = pred_tcga,
-                       this_subtype = "Mes",
-                       subtype_class = "5_class",
-                       plot_title = "TCGA", 
-                       return_data = FALSE)
+  pdf(file = paste0(out_path, "delta_", plot_title, "_uro.pdf"), width = 8, height = 7)
+  print(uro_plot)
+  dev.off()
+  pdf(file = paste0(out_path, "delta_", plot_title, "_gu.pdf"), width = 8, height = 7)
+  print(gu_plot)
+  dev.off()
+  pdf(file = paste0(out_path, "delta_", plot_title, "_basq.pdf"), width = 8, height = 7)
+  print(basq_plot)
+  dev.off()
+  pdf(file = paste0(out_path, "delta_", plot_title, "_mes.pdf"), width = 8, height = 7)
+  print(mes_plot)
+  dev.off()
+  pdf(file = paste0(out_path, "delta_", plot_title, "_scne.pdf"), width = 8, height = 7)
+  print(scne_plot)
+  dev.off()
+  pdf(file = paste0(out_path, "delta_", plot_title, "_uroa.pdf"), width = 8, height = 7)
+  print(uroa_plot)
+  dev.off()
+  pdf(file = paste0(out_path, "delta_", plot_title, "_urob.pdf"), width = 8, height = 7)
+  print(urob_plot)
+  dev.off()
+  pdf(file = paste0(out_path, "delta_", plot_title, "_uroc.pdf"), width = 8, height = 7)
+  print(uroc_plot)
+  dev.off()
+  pdf(file = paste0(out_path, "delta_", plot_title, "_class_5.pdf"), width = 8, height = 7)
+  print(class_5_plot)
+  dev.off()
+  pdf(file = paste0(out_path, "delta_", plot_title, "_class_7.pdf"), width = 8, height = 7)
+  print(class_7_plot)
+  dev.off()
+  
+  plot_data_5 = plot_deltas(these_predictions = these_predictions, 
+                          subtype_class = "5_class", 
+                          this_subtype = NULL,
+                          return_data = TRUE)
+  
+  plot_data_7 = plot_deltas(these_predictions = these_predictions, 
+                          subtype_class = "7_class", 
+                          this_subtype = NULL,
+                          return_data = TRUE)
+  
+  desired_order_5 <- c("Uro", "GU", "BaSq", "Mes", "ScNE")
+  plot_data_5$subtype <- factor(plot_data_5$subtype, levels = desired_order_5)
+  
+  desired_order_7 <- c("UroA", "UroB", "UroC")
+  plot_data_7$subtype <- factor(plot_data_7$subtype, levels = desired_order_7)
+  
+  
+  pdf(file = paste0(out_path, "delta_dist_box_", plot_title, "_5_class.pdf"), width = 8, height = 7)
+  print(ggplot(plot_data_5, aes(x = subtype, y = delta, fill = subtype)) +
+    geom_boxplot() +
+    ylim(0, 1) +
+    scale_fill_manual(values = lund_colors$lund_colors) +
+    theme_bw() +
+    theme(panel.background = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          plot.background = element_blank()) +
+    labs(title = paste0(plot_title, " 5_class"),
+         subtitle = "Distribution of Delta Values by Subtype",
+         x = "Subtype",
+         y = "Delta"))
+  dev.off()
+  
+  pdf(file = paste0(out_path, "delta_dist_box_", plot_title, "_7_class.pdf"), width = 8, height = 7)
+  print(ggplot(plot_data_7, aes(x = subtype, y = delta, fill = subtype)) +
+    geom_boxplot() +
+    ylim(0, 1) +
+    scale_fill_manual(values = lund_colors$lund_colors) +
+    theme_bw() +
+      theme(panel.background = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.background = element_blank()) +
+    labs(title = paste0(plot_title, " 7_class"),
+         subtitle = "Distribution of Delta Values by Subtype",
+         x = "Subtype",
+         y = "Delta"))
+  dev.off()
+  
+  pdf(file = paste0(out_path, "delta_dens_", plot_title, "_5_class.pdf"), width = 8, height = 7)
+  print(ggplot(plot_data_5, aes(x = delta, fill = subtype)) +
+    geom_density(alpha = 0.6) +
+    scale_fill_manual(values = lund_colors$lund_colors) +
+    theme_bw() +
+      theme(panel.background = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.background = element_blank()) +
+    labs(title = paste0(plot_title, " 5_class"),
+         subtitle = "Delta Values Density by Subtype",
+         x = "Delta",
+         y = "Density"))
+  dev.off()
+  
+  pdf(file = paste0(out_path, "delta_dens_", plot_title, "_7_class.pdf"), width = 8, height = 7)
+  print(ggplot(plot_data_7, aes(x = delta, fill = subtype)) +
+    geom_density(alpha = 0.6) +
+    scale_fill_manual(values = lund_colors$lund_colors) +
+    theme_bw() +
+      theme(panel.background = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.background = element_blank()) +
+    labs(title = paste0(plot_title, " 7_class"),
+         subtitle = "Delta Values Density by Subtype",
+         x = "Delta",
+         y = "Density"))
+  dev.off()
+  
+  pdf(file = paste0(out_path, "scatter_", plot_title, "_5_class.pdf"), width = 7, height = 8)
+  print(ggplot(plot_data_5, aes(x = sample_id, y = delta, color = subtype)) +
+    geom_point() +
+    ylim(0, 1) +    
+    scale_color_manual(values = lund_colors$lund_colors) +
+    theme_bw() +
+      theme(panel.background = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.background = element_blank()) +
+    theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
+    labs(title = paste0(plot_title, " 5_class"),
+         subtitle = "Scatter Plot of Delta Values by Subtype",
+         x = "Sample ID",
+         y = "Delta"))
+  dev.off()
+  
+  
+  pdf(file = paste0(out_path, "scatter_", plot_title, "_7_class.pdf"), width = 7, height = 8)
+  print(ggplot(plot_data_7, aes(x = sample_id, y = delta, color = subtype)) +
+    geom_point() +
+    ylim(0, 1) +  
+    scale_color_manual(values = lund_colors$lund_colors) +
+    theme_bw() +
+      theme(panel.background = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.background = element_blank()) +
+    theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
+    labs(title = paste0(plot_title, " 7_class"),
+         subtitle = "Scatter Plot of Delta Values by Subtype",
+         x = "Sample ID",
+         y = "Delta"))
+  dev.off()
+  
+  pdf(file = paste0(out_path, "mean_", plot_title, "_5_class.pdf"), width = 7, height = 8)
+  print(ggplot(plot_data_5, aes(x = subtype, y = delta, fill = subtype)) +
+    stat_summary(fun = mean, geom = "bar") +
+    scale_fill_manual(values = lund_colors$lund_colors) +
+    ylim(0, 1) +
+    theme_bw() +
+      theme(panel.background = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.background = element_blank()) +
+    labs(title = paste0(plot_title, " 5_class"),
+         subtitle = "Mean Delta Values by Subtype",
+         x = "Subtype",
+         y = "Mean Delta"))
+  dev.off()
+  
+  pdf(file = paste0(out_path, "mean_", plot_title, "_7_class.pdf"), width = 7, height = 8)
+  print(ggplot(plot_data_7, aes(x = subtype, y = delta, fill = subtype)) +
+    stat_summary(fun = mean, geom = "bar") +
+    scale_fill_manual(values = lund_colors$lund_colors) +
+    ylim(0, 1) +
+    theme_bw() +
+      theme(panel.background = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.background = element_blank()) +
+    labs(title = paste0(plot_title, " 7_class"),
+         subtitle = "Mean Delta Values by Subtype",
+         x = "Subtype",
+         y = "Mean Delta"))
+  dev.off()
+  
+  pdf(file = paste0(out_path, "jitter_", plot_title, "_5_class.pdf"), width = 7, height = 8)
+  print(ggplot(plot_data_5, aes(x = subtype, y = delta, color = subtype)) +
+    geom_jitter(width = 0.2) +
+    ylim(0, 1) +  
+    scale_color_manual(values = lund_colors$lund_colors) +
+    theme_bw() +
+      theme(panel.background = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.background = element_blank()) +
+    labs(title = paste0(plot_title, " 5_class"),
+         subtitle = "Jitter Plot of Delta Values by Subtype",
+         x = "Subtype",
+         y = "Delta"))
+  dev.off()
+  
+  pdf(file = paste0(out_path, "jitter_", plot_title, "_7_class.pdf"), width = 7, height = 8)
+  print(ggplot(plot_data_7, aes(x = subtype, y = delta, color = subtype)) +
+    geom_jitter(width = 0.2) +
+    ylim(0, 1) +  
+    scale_color_manual(values = lund_colors$lund_colors) +
+    theme_bw() +
+      theme(panel.background = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.background = element_blank()) +
+    labs(title = paste0(plot_title, " 7_class"),
+         subtitle = "Jitter Plot of Delta Values by Subtype",
+         x = "Subtype",
+         y = "Delta"))
+  dev.off()
+  
+  facet_data_5 <- plot_data_5 %>%
+    select(-delta) %>%
+    gather(key = "subtype_score", value = "score", -sample_id, -subtype)
+  
+  facet_data_7 <- plot_data_7 %>%
+    select(-delta) %>%
+    gather(key = "subtype_score", value = "score", -sample_id, -subtype)
+  
+  pdf(file = paste0(out_path, "sub_score_facet_", plot_title, "_5_class.pdf"), width = 8, height = 7)
+  print(ggplot(facet_data_5, aes(x = sample_id, y = score)) +
+    geom_point(aes(color = subtype)) +
+    ylim(0, 1) +
+    scale_color_manual(values = lund_colors$lund_colors) +
+    theme_bw() +
+      theme(panel.background = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.background = element_blank()) +
+    theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
+    facet_wrap(~ subtype_score, scales = "free_x") +
+    labs(title = paste0(plot_title, " 5_class"),
+         subtitle = "Subtype Prediction Score by Subtype",
+         x = "Sample ID",
+         y = "Score"))
+  dev.off()
+  
+  pdf(file = paste0(out_path, "sub_score_facet_", plot_title, "_7_class.pdf"), width = 8, height = 7)
+  print(ggplot(facet_data_7, aes(x = sample_id, y = score)) +
+    geom_point(aes(color = subtype)) +
+    ylim(0, 1) +
+    scale_color_manual(values = lund_colors$lund_colors) +
+    theme_bw() +
+      theme(panel.background = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.background = element_blank()) +
+    theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
+    facet_wrap(~ subtype_score, scales = "free_x") +
+    labs(title = paste0(plot_title, " 7_class"),
+         subtitle = "Subtype Prediction Score by Subtype",
+         x = "Sample ID",
+         y = "Score"))
+  dev.off()
+}
 
-scne_plot = plot_deltas(these_predictions = pred_tcga,
-                        this_subtype = "ScNE",
-                        subtype_class = "5_class",
-                        plot_title = "TCGA", 
-                        return_data = FALSE)
+plot_wrapper(these_predictions = pred_tcga, 
+             plot_title = "tcga", 
+             out_path = "lundtax_project/out/figures/tcga/")  
 
-uroa_plot = plot_deltas(these_predictions = pred_tcga,
-                        this_subtype = "UroA",
-                        subtype_class = "7_class",
-                        plot_title = "TCGA", 
-                        return_data = FALSE)
+plot_wrapper(these_predictions = pred_uc_genome, 
+             plot_title = "uc_genome", 
+             out_path = "lundtax_project/out/figures/uc/")  
 
-urob_plot = plot_deltas(these_predictions = pred_tcga,
-                        this_subtype = "UroB",
-                        subtype_class = "7_class",
-                        plot_title = "TCGA", 
-                        return_data = FALSE)
+plot_wrapper(these_predictions = pred_leeds, 
+             plot_title = "leeds", 
+             out_path = "lundtax_project/out/figures/leeds/")  
 
-uroc_plot = plot_deltas(these_predictions = pred_tcga,
-                        this_subtype = "UroC",
-                        subtype_class = "7_class",
-                        plot_title = "TCGA", 
-                        return_data = FALSE)
-
-pdf(file = "lundtax_project/out/figures/delta_tcga_uro.pdf" ,width = 11, height = 7)
-print(uro_plot)
-dev.off()
-pdf(file = "lundtax_project/out/figures/delta_tcga_gu.pdf" ,width = 11, height = 7)
-print(gu_plot)
-dev.off()
-pdf(file = "lundtax_project/out/figures/delta_tcga_basq.pdf" ,width = 11, height = 7)
-print(basq_plot)
-dev.off()
-pdf(file = "lundtax_project/out/figures/delta_tcga_mes.pdf" ,width = 11, height = 7)
-print(mes_plot)
-dev.off()
-pdf(file = "lundtax_project/out/figures/delta_tcga_scne.pdf" ,width = 11, height = 7)
-print(scne_plot)
-dev.off()
-pdf(file = "lundtax_project/out/figures/delta_tcga_uroa.pdf" ,width = 11, height = 7)
-print(uroa_plot)
-dev.off()
-pdf(file = "lundtax_project/out/figures/delta_tcga_urob.pdf" ,width = 11, height = 7)
-print(urob_plot)
-dev.off()
-pdf(file = "lundtax_project/out/figures/delta_tcga_uroc.pdf" ,width = 11, height = 7)
-print(uroc_plot)
-dev.off()
-pdf(file = "lundtax_project/out/figures/delta_tcga_class_5.pdf" ,width = 11, height = 7)
-print(class_5_plot)
-dev.off()
-pdf(file = "lundtax_project/out/figures/delta_tcga_class_7.pdf" ,width = 11, height = 7)
-print(class_7_plot)
-dev.off()
-
+ 
